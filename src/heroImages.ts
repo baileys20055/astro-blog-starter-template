ecfe3f365bc6c19919f168ab6d594f87c46de0a1
Shae Bailey <81445894+baileys20055@users.noreply.github.com>
2025-07-14 01:17:46 -0400

    Add hero images for blog placeholders with dynamic imports

---
+++ src/heroImages.ts
@@ -0,0 +1,17 @@
+export const heroImages = {
+  "/blog-placeholder-1.webp": await import(
+    "./assets/images/blog-placeholder-1.webp"
+  ),
+  "/blog-placeholder-2.webp": await import(
+    "./assets/images/blog-placeholder-2.webp"
+  ),
+  "/blog-placeholder-3.webp": await import(
+    "./assets/images/blog-placeholder-3.webp"
+  ),
+  "/blog-placeholder-4.webp": await import(
+    "./assets/images/blog-placeholder-4.webp"
+  ),
+  "/blog-placeholder-5.webp": await import(
+    "./assets/images/blog-placeholder-5.webp"
+  ),
+};
